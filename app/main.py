a612b9f91356f874ce4cbcb74eeeb7ae55972b9f
silentbhop <ravil.rinatovich@gmail.com>
2026-04-24 23:16:10 +0500

    add routers in main file

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
 from fastapi import FastAPI
 
+from app.auth.router import router as auth_router
+from app.users.router import router as users_router
+from app.reports.router import router as reports_router
+from app.comments.router import router as comments_router
+from app.categories.router import router as categories_router
+
+
 app = FastAPI()
+
+app.include_router(auth_router)
+app.include_router(users_router)
+app.include_router(reports_router)
+app.include_router(comments_router)
+app.include_router(categories_router)
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
